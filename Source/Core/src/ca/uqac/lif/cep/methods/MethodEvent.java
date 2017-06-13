d5973391b746d69d19775d9616401384b7c4e2ad
Sylvain Hallé <shalle@acm.org>
2017-06-13 16:54:26 -0400

    Method calls with arguments

--- Source/Core/src/ca/uqac/lif/cep/methods/MethodEvent.java
+++ Source/Core/src/ca/uqac/lif/cep/methods/MethodEvent.java
@@ -1,3 +1,20 @@
+/*
+    BeepBeep palette for analyzing traces of method calls
+    Copyright (C) 2017 Raphaël Khoury, Sylvain Hallé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ca.uqac.lif.cep.methods;
 
 public abstract class MethodEvent 
@@ -5,8 +22,8 @@ public abstract class MethodEvent
 	/**
 	 * The name of the method considered by this event
 	 */
-	protected String m_methodName;
-	
+	protected final String m_methodName;
+		
 	public MethodEvent(String name)
 	{
 		super();
@@ -23,9 +40,25 @@ public abstract class MethodEvent
 	 */
 	public static class MethodCall extends MethodEvent
 	{
-		public MethodCall(String name)
+		/**
+		 * The parameters associated to this method call
+		 */
+		protected final Object[] m_arguments;
+
+		public MethodCall(String name, Object ... parameters)
 		{
 			super(name);
+			m_arguments = parameters;
+		}
+		
+		/**
+		 * Gets the <i>i</i>-th argument of this method call
+		 * @param index The index of the argument to retrieve
+		 * @return The argument
+		 */
+		public Object getArgument(int index)
+		{
+			return m_arguments[index];
 		}
 		
 		@Override
